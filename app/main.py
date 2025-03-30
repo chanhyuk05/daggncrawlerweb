47bc86ff834a98494957b45d927fa7cf88cfda3d
chanhyuk05 <24sunrin106@sunrint.hs.kr>
2025-03-31 00:31:35 +0900

    dsnjahoi

--- app/main.py
+++ app/main.py
@@ -2,21 +2,30 @@ import json
 import os
 import platform
 import time
-
 import click
 import questionary
 
-from app.crawl import get_items
-from app.location import get_location
-from app.save import save_as_excel
+from fastapi import *
+from crawl import get_items
+from location import get_location
+from save import save_as_excel
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
 
-@click.command()
-def crawl():
-    region_keyword = questionary.text("크롤링할 지역명을 입력하세요:").ask()
-    
-    is_multiple_region = questionary.confirm("여러 지역을 크롤링하시겠습니까?", default=False).ask()
-    
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/crawl")
+async def crawl(region_keyword: str, is_multiple_region: bool = False, amount: int = 10, keyword: str = None, start_price: int = None, end_price: int = None):
     if not is_multiple_region:
         location_choices = get_location(region_keyword)
         
@@ -27,55 +36,38 @@ def crawl():
         if len(location_choices) == 1:
             loc = location_choices[0]
             location_id = f"{loc['name']}-{loc['id']}"
-        else:
-            selected_location = questionary.select(
-                "지역을 선택하세요:",
-                choices=[loc["label"] for loc in location_choices]
-            ).ask()
-            selected_loc = next(loc for loc in location_choices if loc["label"] == selected_location)
-            location_id = f"{selected_loc['name']}-{selected_loc['id']}"
             
     else:
         location_id = []
         
         location_choices = get_location(region_keyword)
-        
-        if not location_choices:
-            print("해당 지역에 대한 결과가 없습니다.")
-            return
 
         selected_locations = location_choices
         
         for selected_location in selected_locations:
             location_id.append(f"{selected_location['name']}-{selected_location['id']}")
 
-    amount = int(questionary.text("크롤링할 아이템 수를 입력하세요 (기본값 10):", default="10").ask())
-    keyword = questionary.text("검색어를 입력하세요:").ask()
-    has_price_range = questionary.confirm("가격대를 입력하시겠습니까?", default=False).ask()
-    
-    if not has_price_range:
-        start_price = None
-        end_price = None
-    else:
-        start_price = int(questionary.text("최소 가격을 입력하세요 (기본값 0):", default='0').ask())
-        end_price = int(questionary.text("최대 가격을 입력하세요 (기본값 0):", default='0').ask())
-
     results = get_items(location_id, amount, keyword, start_price, end_price)
-    
+    return results
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# @click.command()
+
     # print(json.dumps(results, ensure_ascii=False , indent=2))
+    #==================================엑셀 파일 저장==================================================
+    # save_as_excel(results)
     
-    save_as_excel(results)
+    # print(f"output.xlsx 파일로 저장되었습니다.")
     
-    print(f"output.xlsx 파일로 저장되었습니다.")
-    
-    output_file = "output.xlsx"
-    if platform.system() == "Darwin":  # macOS
-        os.system(f"open {output_file}")
-    elif platform.system() == "Windows":
-        os.startfile(output_file)
-    else:  # Linux
-        os.system(f"xdg-open {output_file}")
-
-
+    # output_file = "output.xlsx"
+    # if platform.system() == "Darwin":  # macOS
+    #     os.system(f"open {output_file}")
+    # elif platform.system() == "Windows":
+    #     os.startfile(output_file)
+    # else:  # Linux
+    #     os.system(f"xdg-open {output_file}")
 if __name__ == '__main__':
     crawl()
